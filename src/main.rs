a8ac631787858b83e4ccfd1195cefa29b5ee76d7
Frederik Bode <thorbensen@gmail.com>
2023-09-13 19:38:29 +0200

    feat: started work on simple webserver

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+        println!("Connection established!");
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let reader = BufReader::new(&stream);
+    //  let mut writer = BufWriter::new(&stream);
+
+    let http_request: Vec<_> = reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+}
